b395eea169ee9d2e02caba9860d3146afd133278
vdyshkant <develop@fastmail.co.uk>
2021-03-29 00:43:28 +0300

    added Item component

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Item = () => {
+  return (
+    <div style={{width: '120px', height: '100px',}}>
+      elem
+    </div>
+  );
+};
+
+export default Item;
--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Item from './Item'
 
 const List = () => {
   const n = 8
   return (
     <>
-      {[...Array(n)].map((e, i) => <div key={i} style={{width: '120px', height: '100px',}}>elem {i}</div>) }
+      {[...Array(n)].map((e, i) => <Item key={i} style={{width: '120px', height: '100px',}} />) }
     </>
   );
 };

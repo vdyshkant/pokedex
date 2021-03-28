53979eb329be2584f4216cf98a6b3702770106d9
vdyshkant <develop@fastmail.co.uk>
2021-03-29 01:33:40 +0300

    added global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createGlobalStyle} from 'styled-components'
+const Global = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  font-family: 'Roboto', sans-serif;
+}
+`
 
 ReactDOM.render(
-    <App />,
+  <>
+    <Global />
+    <App />
+  </>,
   document.getElementById('root')
 );

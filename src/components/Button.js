8ba106efc60de549317892c97b9663614ff6cfd4
vdyshkant <develop@fastmail.co.uk>
2021-03-29 00:44:00 +0300

    added Button component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button>
+      Load More
+    </button>
+  );
+};
+
+export default Button;
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Item from './Item'
+import Button from './Button'
 
 const List = () => {
   const n = 8
   return (
     <>
       {[...Array(n)].map((e, i) => <Item key={i} style={{width: '120px', height: '100px',}} />) }
+
+      <Button />
     </>
   );
 };

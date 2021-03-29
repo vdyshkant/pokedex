c88f78e532e724ff2edf88621f10cdca45b48bbb
vdyshkant <develop@fastmail.co.uk>
2021-03-29 03:05:33 +0300

    added initial <Header> styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const TopRow = styled.div`
+  border: 2px solid grey;
+  text-align: center;
+  width: 80%;
+  margin: 0 0 20px auto;
+  
+  h1 {
+    font-weight: 400;
+    padding: 5px 0;
+  }
+`
 
 const Header = ({title}) => {
   return (
-    <div>
+    <TopRow>
       <h1>{title}</h1>
-    </div>
+    </TopRow>
   );
 };
 

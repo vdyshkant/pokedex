4a8a5330d6604b078a76bd9ff9fa66a5bec573fd
vdyshkant <develop@fastmail.co.uk>
2021-03-29 03:02:33 +0300

    added initial styles for <Button>

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  width: 100%;
+  height: 40px;
+  background: dodgerblue;
+  border-radius: 5px;
+  border: 0;
+  outline: 0;
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: 700;
+  cursor: pointer;
+  
+  @media (min-width:576px) {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+`
 
 const Button = () => {
   return (
-    <button>
+    <ButtonStyled>
       Load More
-    </button>
+    </ButtonStyled>
   );
 };
 

412a38b355492b78e2af409333aa06834b029957
vdyshkant <develop@fastmail.co.uk>
2021-03-30 04:06:11 +0300

    CSS minor fix

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const ListContainer = styled.div`
-  display: flex;
-  flex-flow:row wrap;
-  padding-bottom: 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+  padding-bottom: 60px;
   
   outline: 2px solid gold;
     

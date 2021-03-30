6d908dc4bff8ec8feb185721891ae8342c48ab13
vdyshkant <develop@fastmail.co.uk>
2021-03-31 01:00:40 +0300

    added action creator, refactoring

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,32 +1,45 @@
 const defaultState = {
-  initialList: null,
-  items: [],
+  fetchUrl: 'https://pokeapi.co/api/v2/pokemon/?limit=12',
+  itemCollection: [],
   isDescriptionActive: false,
   currentPokemonId: null,
   currentCategory: null,
 }
 
+const MAKE_DESCRIPTION_ACTIVE = "MAKE_DESCRIPTION_ACTIVE"
+const SET_CURRENT_POKEMON_ID = "SET_CURRENT_POKEMON_ID"
+const UPDATE_COLLECTION = "UPDATE_COLLECTION"
+const CURRENT_CATEGORY = "CURRENT_CATEGORY"
+const UPDATE_URL = "UPDATE_URL"
+
 export const reducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "ADD_INITIAL_POKEMON_LIST":
-      return {...state, initialList: action.payload}
 
-    case "MAKE_DESCRIPTION_ACTIVE":
+    case MAKE_DESCRIPTION_ACTIVE:
       return {...state, isDescriptionActive: true}
 
-    case "SET_CURRENT_POKEMON_ID":
+    case SET_CURRENT_POKEMON_ID:
       return {...state, currentPokemonId: action.payload}
 
-    case "ADD_POKEMON_DATA_TO_COLLECTION":
+    case UPDATE_COLLECTION:
       return {
         ...state,
-        items: [...state.items, ...action.payload]
+        itemCollection: [...state.itemCollection, ...action.payload]
       }
 
-    case "CURRENT_CATEGORY":
+    case CURRENT_CATEGORY:
       return {...state, currentCategory: action.payload}
+
+    case UPDATE_URL:
+      return {...state, fetchUrl: action.payload}
 
     default:
       return state
   }
 }
+
+export const makeDescriptionActive = (payload) => ({type: MAKE_DESCRIPTION_ACTIVE, payload})
+export const setCurrentPokemonId = (payload) => ({type: SET_CURRENT_POKEMON_ID, payload})
+export const updateCollection = (payload) => ({type: UPDATE_COLLECTION, payload})
+export const setCurrentCategory = (payload) => ({type: CURRENT_CATEGORY, payload})
+export const updateUrl = (payload) => ({type: UPDATE_URL, payload})

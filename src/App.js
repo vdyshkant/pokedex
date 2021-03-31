70a1bb634bebbee97825cec41652433ca15cc4c9
vdyshkant <develop@fastmail.co.uk>
2021-03-31 04:23:27 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const Global = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  @import url('//fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
   font-family: 'Roboto', sans-serif;
 }
 

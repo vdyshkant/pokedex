3d601d39ea5c74434776bce377873344c6276d4d
vdyshkant <develop@fastmail.co.uk>
2021-03-31 13:30:11 +0300

    font issue fix

--- src/App.js
+++ src/App.js
@@ -10,19 +10,16 @@ import {updateUrl, updateCollection} from './store/reducer'
 import {createGlobalStyle} from 'styled-components'
 
 const Global = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
 body {
   min-height: 100vh;
   background: rgba(241,231,103,1);
   background: linear-gradient(135deg, rgba(241,231,103,1) 0%, rgba(241,231,103,1) 61%, rgba(254,182,69,1) 100%);
-}
-
-body > * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  @import url('//fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-  font-family: 'Roboto', sans-serif;
 }
 
 `
